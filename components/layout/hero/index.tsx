1b26cd80c46fa0e9c97d3a154b3ae09674dd035f
Stefano Galiffa <paninos79@yahoo.it>
2024-06-01 20:10:37 +0100

    Add github button to download template

--- components/layout/hero/index.tsx
+++ components/layout/hero/index.tsx
@@ -2,6 +2,7 @@ import { Button } from 'components/button';
 import Image from 'next/image';
 import Link from 'next/link';
 import Arrow from 'public/arrow/arrow';
+import { Github } from 'public/icons/github';
 import Header from '../header';
 
 const HeroContent = () => {
@@ -18,11 +19,19 @@ const HeroContent = () => {
         />
         <div className="flex max-w-2xl flex-col items-center  md:items-start">
           <h2 className="text-center text-3xl font-semibold leading-tight md:text-left lg:text-5xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Free Next.Js template with <span className="whitespace-nowrap">Headless Shopify❤️</span>
           </h2>
-          <Link href="/products">
-            <Button className="mt-8 ">Go to products</Button>
-          </Link>
+          <div className="mt-8 flex flex-col gap-2.5 min-[380px]:flex-row">
+            <Link href="/products">
+              <Button>Go to products</Button>
+            </Link>
+            <Link href="https://github.com/stefanogali/panini-cake" target="_blank">
+              <Button className="flex items-center gap-2">
+                <Github className="fill-white" />
+                Get template
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
